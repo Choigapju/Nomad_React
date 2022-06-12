48fc894abd33e628effb8a1e775e0f75bbe1d722
chgju35 <chgju35@gmail.com>
2022-06-12 18:26:35 +0900

    react-for-begin 커밋

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.")
+  }, []);  //처음 실행 시만 작동하고 작동하지 말아라.
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);  // 'keyword'가 동작할 때만 작동해라.
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);  // 'counter'가 동작할 때만 작동해라.
+  useEffect(() => {
+    console.log("I run when keyword & counter change.");
+  }, [keyword, counter]);  // 'keyword'나 'counter'중 하나가 동작할 때 작동해라.
+  return (
+    <div>
+        <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
+        <h1>{counter}</h1>
+        <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
